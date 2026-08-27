a100266fd5540f829ed90d048a9f3530b3521e04
calabasasprincess369 <makenington@gmail.com>
2026-08-27 12:57:36 -0700

    Update page.js

--- app/demo/page.js
+++ app/demo/page.js
@@ -68,19 +68,19 @@ export default function DemoPage() {
         ) : (
           <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", gap: 14 }}>
             <div>
-              <input placeholder="First name" value={fields.firstName} onChange={setField("firstName")} style={inputStyle} />
+              <input required placeholder="First name" value={fields.firstName} onChange={setField("firstName")} style={inputStyle} />
               {errors.firstName && <p style={errorStyle}>{errors.firstName}</p>}
             </div>
             <div>
-              <input placeholder="Last name" value={fields.lastName} onChange={setField("lastName")} style={inputStyle} />
+              <input required placeholder="Last name" value={fields.lastName} onChange={setField("lastName")} style={inputStyle} />
               {errors.lastName && <p style={errorStyle}>{errors.lastName}</p>}
             </div>
             <div>
-              <input placeholder="University" value={fields.university} onChange={setField("university")} style={inputStyle} />
+              <input required placeholder="University" value={fields.university} onChange={setField("university")} style={inputStyle} />
               {errors.university && <p style={errorStyle}>{errors.university}</p>}
             </div>
             <div>
-              <input placeholder="EMAIL" value={fields.email} onChange={setField("email")} style={{ ...inputStyle, textTransform: "uppercase" }} />
+              <input required type="email" placeholder="EMAIL" value={fields.email} onChange={setField("email")} style={{ ...inputStyle, textTransform: "uppercase" }} />
               {errors.email && <p style={errorStyle}>{errors.email}</p>}
             </div>
 
