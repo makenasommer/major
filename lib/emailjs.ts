6aaf7693b93d86be16779e05cfba0c0357c1a078
Development Team Makena HQ <mpartington@fordham.edu>
2026-08-27 17:29:21 -0700

    fix: add EMAILJS_PARTNERS_TEMPLATE export

--- lib/emailjs.ts
+++ lib/emailjs.ts
@@ -2,3 +2,4 @@ export const EMAILJS_PUBLIC_KEY       = "Ep6Js0bcYcxZHs_w_";
 export const EMAILJS_SERVICE_ID       = "service_6mjz2we";
 export const EMAILJS_DEMO_TEMPLATE    = "template_d8imemo";
 export const EMAILJS_CONTACT_TEMPLATE = "template_y3mk2sh";
+export const EMAILJS_PARTNERS_TEMPLATE = EMAILJS_CONTACT_TEMPLATE;
